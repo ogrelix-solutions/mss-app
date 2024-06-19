367b3b56b4e7431a0b47974add9619b9b7b9eda5
Mukeshvinayak <158171937+Mukeshvinayak@users.noreply.github.com>
2024-06-19 15:04:30 +0530

    push

--- database/migrations/2024_06_10_091632_create_jobs_table.php
+++ database/migrations/2024_06_10_091632_create_jobs_table.php
@@ -10,7 +10,10 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
+
+    
     {
+
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->string('cus_id');
